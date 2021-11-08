74f28e5e7f8e9d82ae043e0d31bb194943f7ee2b
Muhammad5943 <muhammad.aji.putra.mp98@gmail.com>
2021-11-08 12:15:37 +0700

    FIXED: Image Uploaded

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -18,7 +18,10 @@ import {
      ORDER_LIST_FAIL,
      ORDER_DELETE_FAIL,
      ORDER_DELETE_SUCCESS,
-     ORDER_DELETE_REQUEST
+     ORDER_DELETE_REQUEST,
+     ORDER_DELIVER_FAIL,
+     ORDER_DELIVER_SUCCESS,
+     ORDER_DELIVER_REQUEST
 } from "../constants/orderConstants"
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -207,13 +210,14 @@ export const deleteOrder = (orderId) => async (dispatch, getState) => {
      } = getState();
      
      try {
-          const { data } = Axios.delete(`/api/orders/${orderId}`, {
+          const { data } = await Axios.delete(`/api/orders/${orderId}`, {
                headers: { 
                     Authorization: `Bearer ${userInfo.token}` 
                },
           })
           dispatch({ 
-               type: ORDER_DELETE_SUCCESS, payload: data 
+               type: ORDER_DELETE_SUCCESS, 
+               payload: data 
           })
      } catch (error) {
           const message =
@@ -222,6 +226,41 @@ export const deleteOrder = (orderId) => async (dispatch, getState) => {
                     : error.message;
           dispatch({ 
                type: ORDER_DELETE_FAIL, 
+               payload: message 
+          })
+     }
+}
+
+export const deliverOrder = (orderId) => async (dispatch, getState) => {
+     dispatch({ 
+          type: ORDER_DELIVER_REQUEST, 
+          payload: orderId 
+     })
+     const {
+          userSignin: { 
+               userInfo 
+          }
+     } = getState();
+     try {
+          const { data } = Axios.put(
+               `/api/orders/${orderId}/deliver`,
+               {},
+               {
+                    headers: { 
+                         Authorization: `Bearer ${userInfo.token}` 
+                    },
+               })
+          dispatch({ 
+               type: ORDER_DELIVER_SUCCESS, 
+               payload: data 
+          })
+     } catch (error) {
+          const message =
+               error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message;
+          dispatch({ 
+               type: ORDER_DELIVER_FAIL, 
                payload: message 
           })
      }
